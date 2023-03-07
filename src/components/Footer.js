82f531cf11866c7448e3709d2599a06e4d8ed927
Svetlana Andreeva <5231328@gmail.com>
2023-03-07 12:19:24 +0300

    hotfix: data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__copyright" lang="en">&copy; 2023 Mesto Russia</p>
+      <p className="footer__copyright" lang="en">&copy; {new Date().getFullYear()} Mesto Russia</p>
     </footer>
   );
 }
